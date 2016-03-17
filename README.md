61b09565a86e1f3f9fbd1d435047eb0e45e49061
Thiago Temple <vintem@fastmail.com>
2016-03-16 22:05:41 -0400

    Project setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ReactTypescriptWebpackMvcDemo
 A bootstrap application using asp.net mvc, webpack, typescript and reactjs
+
+You should have node, npm webpack and tsd installed globally.
+
+Clone and run:
+
+```
+npm install
+```
---
+++ ReactTypescriptWebpackMvcDemo/Scripts/app/App.js
@@ -0,0 +1,9 @@
+"use strict";
+var React = require('react');
+var react_dom_1 = require('react-dom');
+var movies = ['Star Wars', 'The Matrix', 'Forrest Gump'];
+var App = function (_a) {
+    var movies = _a.movies;
+    return (React.createElement("ul", null, movies.map(function (movie) { return React.createElement("li", null, movie); })));
+};
+react_dom_1.render(React.createElement(App, {movies: movies}), document.getElementById('content'));
